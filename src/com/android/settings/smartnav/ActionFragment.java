8f55ee0680970dc173f0b2039ec3e99293a8e023
SuperDroidBond <superdroidbond@yahoo.com>
2018-11-24 22:52:10 +0530

    Move Nav Bar elements to new nav bar [2/2]

--- src/com/android/settings/smartnav/ActionFragment.java
+++ src/com/android/settings/smartnav/ActionFragment.java
@@ -165,7 +165,7 @@ public class ActionFragment extends SettingsPreferenceFragment implements
         switch (dialogId) {
             case DIALOG_CATEGORY:
             case DIALOG_CUSTOM_ACTIONS:
-                return MetricsEvent.KCUF_SETTINGS;
+                return MetricsEvent.STAG_SETTINGS;
             default:
                 return 0;
         }
@@ -256,6 +256,6 @@ public class ActionFragment extends SettingsPreferenceFragment implements
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.KCUF_SETTINGS;
+        return MetricsProto.MetricsEvent.STAG_SETTINGS;
     }
 }
